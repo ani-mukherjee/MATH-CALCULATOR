5899550200b6968ecd73c272c8a61a7d8d0bcd3a
ani-mukherjee <ani.mukherjee93@gmail.com>
2023-02-23 16:53:29 +0530

    calculator

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-function DeleteMe(){
-    document.getElementById("myresult").value = ""
+function Delete(){
+    document.getElementById("myresult").value = "";
+}
+function back(){
+    let x= document.getElementById("myresult").value;
+    var str = x.toString();
+    x = str.slice(0, str.length - 1);
+    document.getElementById("myresult").value= x; 
 }
 
 function calculator(NewValue){
     document.getElementById("myresult").value += NewValue;
 }
-function Answer(){
+function submit(){
     let a = document.getElementById("myresult").value;
     let b = eval(a);
     document.getElementById("myresult").value= b;
